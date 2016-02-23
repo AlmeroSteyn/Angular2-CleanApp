050807724f1af9802d24ca74d27c6f2e00964c94
AlmeroSteyn <almero.steyn@gmail.com>
2016-02-23 08:41:34 +0100

    Initial commit

---
+++ app/app-boot.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "angular2/platform/browser";
+import {AppRoot} from "./app-root";
+
+bootstrap(AppRoot);
---
+++ app/app-root.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: 'app-root',
+  template: `<div>I am some content</div>`
+})
+
+export class AppRoot {
+
+}
